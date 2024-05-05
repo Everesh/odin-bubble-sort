b56d7c9ee1f431fea1bab38cb0ce19f83f261a8e
Everesh <everesh@proton.me>
2024-05-05 17:51:16 +0200

    Add logic

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,5 +1,19 @@
 def bubble_sort(arr)
 
-    # TO DO
+    sorted = false
+
+    while !sorted
+        sorted = true
+        arr[0..-2].each.with_index do |value, index|
+            if arr[index] > arr[index + 1]
+                tmp = arr[index]
+                arr[index] = arr[index + 1]
+                arr[index + 1] = tmp
+                sorted = false
+            end
+        end
+    end
+
+    arr
 
 end
